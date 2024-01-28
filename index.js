5c7f0d57bf622cab4fb32d48fda61d03506509eb
BekiaD <144695091+BekiaD@users.noreply.github.com>
2024-01-28 19:14:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 import { catsData } from "./data.js"
 
 const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
-function highlightCheckedOption(e){
-    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+getImageBtn.addEventListener('click', getMatchingCatsArray)
 
+
+function highlightCheckedOption(e) {
+    const radios = document.getElementsByClassName('radio')
+    for (let radio of radios) {
+        radio.classList.remove('highlight')
+    }
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
 }
 
+function getMatchingCatsArray(){     
+    if(document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        const isGif = gifsOnlyOption.checked
 
+        const matchingCatsArray = catsData.filter(function(cat){
+            
+            return cat.emotionTags.includes(selectedEmotion)
+        })
+        console.log(matchingCatsArray)
+    }
+}
 
 function getEmotionsArray(cats) {
     const emotionsArray = []
@@ -19,8 +38,8 @@ function getEmotionsArray(cats) {
             // } else {
             //     emotionsArray.push(emotion)
             // }
-            if(!emotionsArray.includes(emotion))
-            emotionsArray.push(emotion)
+            if (!emotionsArray.includes(emotion))
+                emotionsArray.push(emotion)
         }
     }
     return emotionsArray
@@ -48,7 +67,6 @@ function renderEmotionsRadios(cats) {
 }
 
 renderEmotionsRadios(catsData)
-
 
 
 
